1a189fb3a4d9497af36cd729aa52c81761200db1
courtney persons <personscourtney@gmail.com>
2018-09-20 12:57:36 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,13 @@
 x = 10
 def countdown(x)
   while x > 0
-    puts "#{-x} SECOND(S)!"
+    puts "#-{x} SECOND(S)!"
     x -= 1
   end
   "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(1.seconds)
+  
 end
